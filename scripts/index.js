c46431cf52f61e218a3a205d48c20bcfd811349b
Pjotr Savitski <gnum@tlu.ee>
2018-02-09 17:43:32 +0200

    References Oppevara/oppevara#61, editor uses ibrary components.
    Removed any shared styles or scripts and moved those into the special
    library. Removed some dependencies, those would be loaded by the new
    standalone helper library. Renamed style, might not be needed in this
    case but similar names seems to be confusing the listing view.

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@
 
 var H5PEditor = H5PEditor || {};
 
-H5PEditor.widgets.musicCompositionExercises = H5PEditor.MusicCompositionExercises = (function($, JoubelUI) {
+H5PEditor.widgets.musicCompositionExercises = H5PEditor.MusicCompositionExercises = (function($) {
   /**
    * Constructor function
    * @param       {object}   parent   Parent representation
@@ -39,111 +39,13 @@ H5PEditor.widgets.musicCompositionExercises = H5PEditor.MusicCompositionExercise
       this.exercise.close();
     }
 
-    var containerNode = this.$container.get(0);
-    var canvasClassName = 'mainCanvas';
-
-    switch(this.getType()) {
-      case '1.1':
-        this.exercise = recognizeDuration(containerNode, canvasClassName);
-        break;
-      case '1.2':
-        this.exercise = nameDuration(containerNode, canvasClassName);
-        break;
-      case '1.3':
-        this.exercise = findMissingDuration(containerNode, canvasClassName);
-        break;
-      case '1.4':
-        this.exercise = drawBarlines(containerNode, canvasClassName);
-        break;
-      case '1.5':
-        this.exercise = findTime(containerNode, canvasClassName);
-        break;
-      case '2.5':
-        this.exercise = describeNote("treble", containerNode, canvasClassName);
-        break;
-      case '2.6':
-        this.exercise = describeNote("bass", containerNode, canvasClassName);
-        break;
-      case '2.7':
-        this.exercise = noteFromNoteName("treble", containerNode, canvasClassName);
-        break;
-      case '2.8':
-        this.exercise = noteFromSyllable("treble", containerNode, canvasClassName);
-        break;
-      case '2.9':
-        this.exercise = noteFromNotation("treble", containerNode, canvasClassName);
-        break;
-      case '2.10':
-        this.exercise = noteFromKeyboard("treble", containerNode, canvasClassName);
-        break;
-      case '2.11':
-        this.exercise = noteFromNoteName("bass", containerNode, canvasClassName);
-        break;
-      case '2.12':
-        this.exercise = noteFromSyllable("bass", containerNode, canvasClassName);
-        break;
-      case '2.13':
-        this.exercise = noteFromNotation("bass", containerNode, canvasClassName);
-        break;
-      case '2.14':
-        this.exercise = noteFromKeyboard("bass", containerNode, canvasClassName);
-        break;
-      case '2.15':
-        this.exercise = enharmonism("name", containerNode, canvasClassName);
-        break;
-      case '2.16':
-        this.exercise = enharmonism("syllable", containerNode, canvasClassName);
-        break;
-      case '2.17':
-        this.exercise = changeClef("bass", containerNode, canvasClassName);
-        break;
-      case '2.18':
-        this.exercise = changeClef("treble", containerNode, canvasClassName);
-        break;
-      case '2.19':
-        this.exercise = octaveFromNotation("treble", containerNode, canvasClassName);
-        break;
-      case '2.20':
-        this.exercise = octaveFromNotation("bass", containerNode, canvasClassName);
-        break;
-      case '3.1':
-        this.exercise = buildInterval("treble", "up", containerNode, canvasClassName);
-        break;
-      case '3.2':
-        this.exercise = buildInterval("treble", "down", containerNode, canvasClassName);
-        break;
-      case '3.3':
-        this.exercise = buildInterval("bass", "up", containerNode, canvasClassName);
-        break;
-      case '3.4':
-        this.exercise = buildInterval("bass", "down", containerNode, canvasClassName);
-        break;
-      case '3.5':
-        this.exercise = buildChord("treble", "up", containerNode, canvasClassName);
-        break;
-      case '3.6':
-        this.exercise = buildChord("treble", "down", containerNode, canvasClassName);
-        break;
-      case '3.7':
-        this.exercise = buildChord("bass", "up", containerNode, canvasClassName);
-        break;
-      case '3.8':
-        this.exercise = buildChord("bass", "down", containerNode, canvasClassName);
-        break;
-      case '7.1':
-        this.exercise = recognizeKeySignature(containerNode, canvasClassName);
-        break;
-      case '7.2':
-        this.exercise = buildScale("major", containerNode, canvasClassName);
-        break;
-      case '7.3':
-        this.exercise = nameKey("major", containerNode, canvasClassName);
-        break;
-      case '7.4':
-        this.exercise = nameKey("minor", containerNode, canvasClassName);
-        break;
-      default:
+    try {
+      this.exercise = H5P.MusicCompositionExercisesLibrary.createExerciseInstance(this.getType(), this.$container.get(0), 'mainCanvas');
+    }
+    catch(err) {
+      if ( err === 'invalidExerciseType' ) {
         alert(H5PEditor.t('H5PEditor.MusicCompositionExercises', 'invalidExerciseType', {}));
+      }
     }
   };
 
@@ -182,166 +84,7 @@ H5PEditor.widgets.musicCompositionExercises = H5PEditor.MusicCompositionExercise
       style: 'display:none'
     });
 
-    // TODO This has to move somewhere
-    $('<h2>', {
-      'class': 'exerciseTitle'
-    }).appendTo(self.$exercisePreviewContainer);
-    $('<p>', {
-      'class': 'description'
-    }).appendTo(self.$exercisePreviewContainer);
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    JoubelUI.createButton({
-      'class': 'h5pf-music-compositon-exercises-play',
-      'html': 'Mängi',
-      'on': {
-        'click': function() {
-          self.exercise.play();
-        }
-      },
-      'appendTo': self.$exercisePreviewContainer
-    });
-    $('<div>', {
-      'class': 'mainCanvas'
-    }).appendTo(self.$exercisePreviewContainer);
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-renew',
-      'html': 'Uuenda',
-      'on': {
-        'click': function() {
-          self.$feedback.html('');
-          self.exercise.renew();
-        }
-      },
-      'appendTo': self.$exercisePreviewContainer
-    });
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    $('<span>', {
-      'class': 'question'
-    }).appendTo(self.$exercisePreviewContainer);
-    $('<span>', {
-      'class': 'responseDiv'
-    }).appendTo(self.$exercisePreviewContainer);
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-reply',
-      'html': 'Vasta',
-      'on': {
-        'click': function() {
-          self.exercise.checkResponse();
-        }
-      },
-      'appendTo': self.$exercisePreviewContainer
-    });
-    self.$feedback = $('<p>', {
-      'class': 'feedback'
-    }).appendTo(self.$exercisePreviewContainer);
-    self.$attempts = $('<label>', {
-      'class': 'attempts',
-      'text': '0'
-    });
-    $('<span>', {
-      'text': 'Katseid: '
-    }).append(self.$attempts).appendTo(self.$exercisePreviewContainer);
-    self.$score = $('<label>', {
-      'class': 'score',
-      'text': '0'
-    });
-    $('<span>', {
-      'text': '. Neist õigeid vastuseid: '
-    }).append(self.$score).appendTo(self.$exercisePreviewContainer);
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-reset',
-      'html': 'Nulli',
-      'on': {
-        'click': function() {
-          self.exercise.attempts = 0;
-          self.exercise.score = 0;
-          self.$attempts.text('0');
-          self.$score.text('0');
-        }
-      },
-      'appendTo': self.$exercisePreviewContainer
-    });
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    var resultsElement = $('<div>', {
-      'html': 'Kui tunned, et oled valmis <b>testi sooritama</b>, vajuta: ',
-      'class': 'resuts'
-    }).appendTo(self.$exercisePreviewContainer);
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-show-test showTestButton',
-      'html': 'Test',
-      'on': {
-        'click': function() {
-          self.$testDiv.css('visibility', 'visible');
-        }
-      },
-      'appendTo': resultsElement
-    });
-    $('<br>').appendTo(self.$exercisePreviewContainer);
-    self.$testDiv = $('<div>', {
-      'class': 'testDiv',
-      'style': 'visibility:hidden;',
-      'text': 'Kas soovite tulemuse salvestada pdf faili (nt esitamiseks õpetajale)? '
-    }).appendTo(self.$exercisePreviewContainer);
-
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-make-pdf-button makePdfButton',
-      'html': 'Jah',
-      'on': {
-        'click': function() {
-          self.$pdfDiv.css('visibility', 'visible');
-          self.exercise.saveToPdf = true;
-        }
-      },
-      'appendTo': self.$testDiv
-    });
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-hide-pdf-button hidePdfButton',
-      'html': 'Ei',
-      'on': {
-        'click': function() {
-          self.$pdfDiv.css('visibility', 'hidden');
-          self.exercise.saveToPdf = false;
-        }
-      },
-      'appendTo': self.$testDiv
-    });
-    self.$pdfDiv = $('<div>', {
-      'class': 'h5p-music-compositon-exercises-pdf-div pdfDiv',
-      'style': 'visibility:hidden;',
-      'text': 'Nimi: '
-    }).append($('<input>', {
-      'type': 'text',
-      'class': 'name',
-      'size': '20'
-    })).appendTo(self.$testDiv);
-    self.$testDiv.append('Küsimus nr <b><label class="questionNumber">0</label></b>. Aega on: <b><label  class="timer">0</label></b> sekundit.<br> Aega kulunud kokku: <b><label class="totalTestTime">0</label></b> sekundit.<br>');
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-start-test startTest',
-      'html': 'Alusta',
-      'on': {
-        'click': function() {
-          self.exercise.startTest();
-        }
-      },
-      'appendTo': self.$testDiv
-    });
-    JoubelUI.createButton({
-      'class': 'h5p-music-compositon-exercises-stop-test stopTest',
-      'html': 'Peata',
-      'on': {
-        'click': function() {
-          self.exercise.stopTest();
-        }
-      },
-      'appendTo': self.$testDiv
-    });
-    $('<br>').appendTo(self.$testDiv);
-    $('<p>').append($('<small>', {
-      'text': 'Teile esitatakse 5 küsimust, mis tuleb ettemääratud aja jooksul vastata. Kui jõuate varem valmis, vajutage lihtsalt "Vasta". Uus küsimus kuvatakse peale eelmise vastamist või aja täitumisel.'
-    })).appendTo(self.$testDiv);
+    H5P.MusicCompositionExercisesLibrary.generateHtml(self, self.$exercisePreviewContainer);
 
     self.$exercisePreviewContainer.appendTo(self.$container);
 
@@ -369,4 +112,4 @@ H5PEditor.widgets.musicCompositionExercises = H5PEditor.MusicCompositionExercise
   };
 
   return MusicCompositionExercises;
-})(H5P.jQuery, H5P.JoubelUI);
+})(H5P.jQuery);
